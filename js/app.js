fb7cf0889f9b9fe0b8541544f58077ab20e2b338
NishiAicchi <ai.espichan@gmail.com>
2017-11-20 13:29:09 -0500

    actualizando CSS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-window.addEventListener('load', funcion(){
-    event.target()
-})
+// declarando variables
+var twitterBox = document.querySelector('twitterbox-center');
+var boxCentral = document.querySelector('box-central');
+var inputBox = document.querySelector('message-creator');
+
+var tweetButton = document.createElement('button');
+tweetButton.classList.add('stileButton');
+tweetButton.textContent = 'twittear';
+
+document.write(tweetButton);
+
+var counter = document.createElement('p');
+counter.classList.add('stilecounter');
+counter.textContent = '140';
+
